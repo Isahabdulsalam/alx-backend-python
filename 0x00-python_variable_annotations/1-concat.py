48e4604e8ceb1666cfb3c77f46162665a2238371
Isah Abdulsalam <Isahabdulsalam416@gmail.com>
2024-10-10 13:33:48 +0000

    task

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+""" a type-annotated function concat that takes a string """
+
 
 def concat(str1: str, str2: str) -> str:
+    """returns a concatenated string"""
     return str1 + str2
