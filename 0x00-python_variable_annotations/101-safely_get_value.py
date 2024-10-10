69d93cc78bc451e111719aa65e3325d6ca4230aa
Isah Abdulsalam <Isahabdulsalam416@gmail.com>
2024-10-10 13:23:34 +0000

    task

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,21 +1,14 @@
 #!/usr/bin/env python3
+"""More involved type annotations"""
+from typing import Mapping, Any, Union, TypeVar
 
-from typing import Any, Mapping, Optional, Union
+T = TypeVar('T')
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[Any, None] = None) -> Union[Any, None]:
-  """
-  Safely retrieves the value from a dictionary for a given key.
-  Returns the default value if the key is not found.
 
-  Args:
-    dct: The dictionary from which to retrieve the value.
-    key: The key whose corresponding value is to be retrieved.
-    default: The value to return if the key is not found.
-
-  Returns:
-    The value associated with the key if found, otherwise the default value.
-  """
-  if key in dct:
-    return dct[key]
-  else:
-    return default
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[T, None] = None) -> Union[Any, T]:
+    """duck typed annotations"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
