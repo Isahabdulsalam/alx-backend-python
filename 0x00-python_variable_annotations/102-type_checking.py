8b26f440b0aeeb635eef8be13b0e618951070f8b
Isah Abdulsalam <Isahabdulsalam416@gmail.com>
2024-10-10 13:22:05 +0000

    task

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,30 +1,23 @@
 #!/usr/bin/env python3
+"""mypy intro"""
+from typing import Tuple, List
 
-from typing import List, Tuple
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
-  """
-  This function takes a tuple and a factor as input and returns a new list
-  where each element of the tuple is repeated 'factor' times.
+    """sumary_line
 
-  Args:
-    lst: The input tuple.
-    factor: The number of times to repeat each element.
-
-  Returns:
-    A new list with the zoomed-in elements.
-  """
-  zoomed_in: List = [
-    item for item in lst
-    for i in range(factor)
-  ]
-  return zoomed_in
+    Keyword arguments:
+    argument -- description
+    Return: return_description
+    """
+    zoomed_in: List = [
+        item for item in lst for i in range(factor)
+    ]
+    return zoomed_in
 
 
 array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
-zoom_3x = zoom_array(array, 3)
 
-print(zoom_2x)
-print(zoom_3x)
+zoom_3x = zoom_array(array, 3)
