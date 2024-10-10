1be0206db949bdc2f2cbd42b1443891216ed40af
Isah Abdulsalam <Isahabdulsalam416@gmail.com>
2024-10-10 13:31:24 +0000

    task

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+"""A type-annotated function that returns the sum of two floats """
+
+
 
 def add(a: float, b: float) -> float:
+    """adds two floats and return sum as float"""
     return a + b
